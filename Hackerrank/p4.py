ac4768e27b0ddbffc2365f4f91b2694d4568b7e0
DhrRas <rastogi.dhruv@gmail.com>
2025-02-11 16:18:17 +0530

    update p4.py

--- Hackerrank/p4.py
+++ Hackerrank/p4.py
@@ -4,9 +4,47 @@ Store them in a list and find the score of the runner-up.'''
 
 if __name__ == '__main__':
     n = int(input())
-    input_list = list(map(int, input().split()))
+    num = list(map(int, input().split()))
 
-    print(input_list)
-    for x in input_list:
-        #print(x)
-        pass
+    for i in range(len(num)-1):
+        if num[i] in num:
+            high_num = num[i]
+            # print(high_num)
+        
+        if high_num > num[i+1]:
+            high_num = high_num 
+            # print(high_num)
+        else:
+            high_num = num[i+1]
+            # print(high_num)
+        
+        if high_num == 9:
+            if high_num > num[i+1]:
+                high_num = high_num
+            else:
+                high_num = num[i+1]
+                
+        print(high_num, end=' ')
+
+        
+        
+
+    
+#    print(new_num)
+        #     print(i)
+        
+        # elif new_num > num[i+1]:
+        #     new_num = num[i+1]
+        #     print(i)
+        
+        # elif new_num > num[i+1]:
+        #     new_num = num[i+1]
+        #     print(i)
+        
+        # elif new_num > num[i+1]:
+        #     new_num = num[i+1]
+        #     print(i)
+
+        # elif new_num > num[i+1]:
+        #     new_num = num[i+1]
+        #     print(i)
