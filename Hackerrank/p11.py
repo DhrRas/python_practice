8db07c6f890e585711e6fc581c8a10f76b5ea7ba
DhrRas <rastogi.dhruv@gmail.com>
2025-02-12 00:01:51 +0530

    update p11.py

--- Hackerrank/p11.py
+++ Hackerrank/p11.py
@@ -1,3 +1,17 @@
-s = input()
-if s.isdigit() == True:
-    print(True)
+''' A set is an unordered collection of elements without duplciates
+entries.
+When printed, iterated or converted into a sequence, its elements will appear in an arbitrary order.'''
+
+def average(array):
+    total = 0
+    for i in range(len(array)):
+        total += array[i]
+        result = total / len(array)
+        avg = '%.3f'%(result)
+    return avg
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
