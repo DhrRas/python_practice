996dd5ee803e7153460d432b6248a78b9ada2638
DhrRas <rastogi.dhruv@gmail.com>
2025-02-13 15:28:11 +0530

    modified programs

--- Hackerrank/p10.py
+++
@@ -1,7 +0,0 @@
-''' Practice to print out star pattern.'''
-
-rows = 5
-for i in range(5):
-    for j in range(3):
-        print('*',end='')
-    print()
--- Hackerrank/p12.py
+++ Hackerrank/p12.py
@@ -1,10 +1,16 @@
 '''In this challenge, the user enters a string and a substring.'''
-
-
 def count_substring(string, sub_string):
     for i in range(0, len(string)):
-        return string.count(sub_string)
-    
+        if string[i] == sub_string[i]:
+            for j in range(0,len(sub_string)):     
+                 word = sub_string[j] + string[j]
+                 word_len = len(word)
+                 
+        else:
+                 break
+        return word_len   
+
+
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
