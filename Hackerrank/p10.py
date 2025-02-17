25e50ae6f4b1d7b8b00f874b4e9a12cc9713daa7
DhrRas <rastogi.dhruv@gmail.com>
2025-02-18 00:23:11 +0530

    update p10.py

--- Hackerrank/p10.py
+++ Hackerrank/p10.py
@@ -2,3 +2,9 @@
 different methods are used to center, right and left adjust the text according to the need.
 '''
 
+if __name__ == '__main__':
+    thickvalue = int(input())
+
+for i in range(1, thickvalue+1):
+    if i % 2 == 1:
+        print(' H'*i)
