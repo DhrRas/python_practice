851f898cdb844945a0d69c37a5fcf7c7124bd608
DhrRas <rastogi.dhruv@gmail.com>
2025-02-17 22:50:50 +0530

    update p9.py

--- Hackerrank/p9.py
+++ Hackerrank/p9.py
@@ -8,21 +8,3 @@ characters, digits etc.'''
 # str.islower() checks if all characters of a string are lowercase or not. 
 # str.isupper() checks if all characters of a string are uppercase or not.
 
-if __name__ == '__main__':
-        s = input().strip()
-        
-        for x in range(len(s)):
-            if s.isalnum() == True:
-                print(True)
-                
-            if s.isalpha() == True:
-                print(True)
-
-            if s.isdigit() == True:
-                print(True)
-
-            if s.islower() == True:
-                print(True)
-
-            if s.isupper() == True:
-                print(True)
