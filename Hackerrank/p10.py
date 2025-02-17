a75c7868e5a557df8c9b909974f97d21ed815c80
DhrRas <rastogi.dhruv@gmail.com>
2025-02-18 00:13:03 +0530

    update p9.py p10.py

--- Hackerrank/p10.py
+++ Hackerrank/p10.py
@@ -1,20 +1,4 @@
-''' Python has built-in string validation methods for basic data.
-it can check if a sring is composed of alphabetical characters,
-alphanumeric characters, digit, etc...'''
+''' In python, a string of text can be aligned left, right and center.
+different methods are used to center, right and left adjust the text according to the need.
+'''
 
-if __name__ == "__main__":
-    string = input()
-        
-    for char in string.split():
-        if string.isalnum() == True:
-            print(True)
-        elif string.isalpha() == True:
-            print(True)
-        elif string.isdigit() == True:
-            print(True)
-        elif string.islower() == True:
-            print(True)
-        elif string.isupper() == True:
-            print(True)
-        else:
-            print(False)
--- Hackerrank/p9.py
+++ Hackerrank/p9.py
@@ -8,3 +8,18 @@ characters, digits etc.'''
 # str.islower() checks if all characters of a string are lowercase or not. 
 # str.isupper() checks if all characters of a string are uppercase or not.
 
+if __name__ == "__main__":
+    string = input()
+        
+    if string.isalnum() == True:
+        print(True)
+    if string.isalpha() == True:
+        print(True)
+    if string.isdigit() == True:
+        print(True)
+    if string.islower() == True:
+        print(True)
+    if string.isupper() == True:
+        print(True)
+    else:
+        print(False)
