f0b4dd48a10d13d4b5cb02f3894c81c5e4e75b1d
DhrRas <rastogi.dhruv@gmail.com>
2025-02-17 23:43:56 +0530

    p.py

---
+++ Hackerrank/p10.py
@@ -0,0 +1,24 @@
+''' Python has built-in string validation methods for basic data.
+it can check if a sring is composed of alphabetical characters,
+alphanumeric characters, digit, etc...'''
+
+if __name__ == "__main__":
+    s = input()
+
+for i in s:
+    pass
+
+    
+    # for char in string:
+    #     if char == string.isalnum():
+    #         print(True)
+    #     elif char == string.isalpha():
+    #         print(True)
+    #     elif char == string.isdigit():
+    #         print(True)
+    #     elif char == string.islower():
+    #         print(True)
+    #     elif char == string.isupper():
+    #         print(True)
+    #     else:
+    #         print(False)
