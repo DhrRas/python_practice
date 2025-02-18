5b2456fdd6d6515a39f44eef6056789285a059f7
DhrRas <rastogi.dhruv@gmail.com>
2025-02-18 20:32:40 +0530

    update p14.py

--- Hackerrank/p14.py
+++ Hackerrank/p14.py
@@ -1,16 +1,20 @@
 if __name__ == '__main__':
 
     string = input()
-    l = d = False
+    
 
     for char in string: # 1st time
         if ord(char) >= 97 or ord(char) >= 65 or char in range(0, 10):
-            l = d = True
+            print(True)
+        
+    print(False)
     
     
     for char in string: # 2nd time
          if ord(char) >= 97 or ord(char) >= 65:
-             l = True
+            print(True)
+
+    print(False)
     
 
     # for char in string: # 3rd time
@@ -30,12 +34,4 @@ if __name__ == '__main__':
     #         print(True)
     #         break
 
-    if l:
-        print(True)
-    
-    elif d:
-        print(True)
-    
-    elif l and d:
-        print(True)
     
