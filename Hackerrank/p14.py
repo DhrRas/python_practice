54b8d5a8630c8c2c794e3caaf356c6257c5d13f0
DhrRas <rastogi.dhruv@gmail.com>
2025-02-19 20:24:13 +0530

    update p9.py

--- Hackerrank/p14.py
+++ Hackerrank/p14.py
@@ -1,37 +1,51 @@
 if __name__ == '__main__':
 
     string = input()
-    
+    alnum = alpha = dig = low = upp = False
 
-    for char in string: # 1st time
-        if ord(char) >= 97 or ord(char) >= 65 or char in range(0, 10):
+# 1st line.
+    for char in string:
+        if char.isalnum():
+            alnum = True
+    
+    if alnum:
+         print(True)
+    else:
+         print(False)
+
+# 2nd line.
+    for char in string:
+        if char.isalpha():
+            alpha = True
+    if alpha:
             print(True)
-        
-    print(False)
-    
-    
-    for char in string: # 2nd time
-         if ord(char) >= 97 or ord(char) >= 65:
+    else:
+            print(False)
+
+# 3rd time.
+    for char in string:
+        if char.isdigit():
+            dig = True       
+    if dig:
             print(True)
+    else:
+            print(False)
 
-    print(False)
-    
+# 4th time.
+    for char in string:
+        if char.islower():
+             low = True
+    if low:
+         print(True)
+    else:
+         print(False)
+ 
+ # 5th time.
+    for char in string:
+         if char.isupper():
+             upp = True
+    if upp:
+        print(True)
+    else:
+        print(False)
 
-    # for char in string: # 3rd time
-    #     if char.isdigit():
-    #         print(True)
-    #     else:
-    #         print(False)
-
-    # for char in string: # 4th time
-    #     if char.islower():
-    #         print(True)
-    #     else:
-    #         print(False)
-
-    # for char in string: # 5th time
-    #     if char.isupper():
-    #         print(True)
-    #         break
-
-    
--- Hackerrank/p9.py
+++ Hackerrank/p9.py
@@ -8,18 +8,54 @@ characters, digits etc.'''
 # str.islower() checks if all characters of a string are lowercase or not. 
 # str.isupper() checks if all characters of a string are uppercase or not.
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     string = input()
-        
-    if string.isalnum() == True:
-        print(True)
-    if string.isalpha() == True:
-        print(True)
-    if string.isdigit() == True:
-        print(True)
-    if string.islower() == True:
-        print(True)
-    if string.isupper() == True:
+    alnum = alpha = dig = low = upp = False
+
+# 1st line.
+    for char in string:
+        if char.isalnum():
+            alnum = True
+    
+    if alnum:
+         print(True)
+    else:
+         print(False)
+
+# 2nd line.
+    for char in string:
+        if char.isalpha():
+            alpha = True
+    if alpha:
+            print(True)
+    else:
+            print(False)
+
+# 3rd time.
+    for char in string:
+        if char.isdigit():
+            dig = True       
+    if dig:
+            print(True)
+    else:
+            print(False)
+
+# 4th time.
+    for char in string:
+        if char.islower():
+             low = True
+    if low:
+         print(True)
+    else:
+         print(False)
+ 
+ # 5th time.
+    for char in string:
+         if char.isupper():
+             upp = True
+    if upp:
         print(True)
     else:
         print(False)
+
