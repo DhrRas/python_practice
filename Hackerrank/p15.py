753e22ba154114cf46bc2d6e52874f1115c32d2d
DhrRas <rastogi.dhruv@gmail.com>
2025-02-20 17:47:52 +0530

    update p15.py

--- Hackerrank/p15.py
+++ Hackerrank/p15.py
@@ -1,13 +1,22 @@
-'''In Python, a string of text can be aligned left, right and center.'''
-# width = 20
-# print(input().ljust(width,'-'))
-# print(input().center(width,'-'))
-# print(input().rjust(width, '-'))
+'''You are aked to ensure that the first and last names of people begin with a capital letter in thier passports.
+For example, alison heck should be capitalized correctly as Alison Heck.'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# complete the solve function below.
+def solve(s):
+    s2 = ' '.join(word.capitalize() for word in s.split())
+    return s2
+    
 
 if __name__ == '__main__':
-
-    thickvalue = int(input())
-
-    for i in range(1, thickvalue):
-        if i % 2 == 1:
-            print('H'.rjust(thickvalue,' '))
+    # fptr = open(os.environ['OUTPUT_PATH'],'w')
+    s = input()
+    result = solve(s)
+    print(result)
