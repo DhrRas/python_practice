90410d9317967960f1657451da54cd9e9b8753a5
DhrRas <rastogi.dhruv@gmail.com>
2025-02-21 16:14:26 +0530

    Update p18.py

--- Hackerrank/p18.py
+++ Hackerrank/p18.py
@@ -4,14 +4,11 @@ we can use the .add() function.'''
 if __name__ == '__main__':
 
     country_stamps = int(input())
-    
-    for i in range(1,country_stamps+1):
-        country_name = input()
-        lst = set(country_name)
-        
-    add_names = lst.add(input())
+    distinct_stamps = list()
 
-        # result = len(lst)
-    print(add_names)
-    print(lst)
-    # print(result)
+    for name in range(country_stamps):
+        name = distinct_stamps.append(input()) 
+        
+    new_distinct = set(distinct_stamps)
+    print(len(new_distinct))
+
