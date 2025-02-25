e879f45036eb418ba393cc1ba7a96f1a7b0e4aed
DhrRas <rastogi.dhruv@gmail.com>
2025-02-25 17:45:52 +0530

    changes made to the p19.py

--- Hackerrank/p19.py
+++ Hackerrank/p19.py
@@ -1,32 +1,37 @@
 ''' Exercise regarding discard() remove() and pop'''
 
 if __name__ == '__main__':
-    integer_list = list()
     no_elements = int(input())
 
-    for i in range(1,no_elements+1): 
-        num = integer_list.append(i)
+    # integer_set = set(integer_list)
+
+    for i in range(no_elements):
+        num = input().split()
+        num2 = list(map(int,num))
+        print(num2,end=' ') 
+         
+    # print()
     
-    integer_set = set(integer_list)
-    for i in integer_set:
-        print(i, end=' ')
+    # for i in range(1, len(integer_list)+1):
+    #      new_num = integer_set.add(i)
+             
+    # print(integer_set)
+    # # print()
+    # no_of_commands = int(input('enter the number of commands: ')) 
 
-    print()
-    no_of_commands = int(input('enter the number of commands: ')) 
+    # for i in range(no_of_commands):
+    #     x = input('').split(' ')
 
-    for i in range(no_of_commands):
-        x = input('').split(' ')
-
-        if x[0] == 'pop':
-            y = 0
-            integer_set.pop()
+    #     if x[0] == 'pop':
+    #         y = 0
+    #         integer_set.pop()
         
-        elif x[0] == 'remove':
-            y = int(x[1])
-            integer_set.remove(y)
+    #     elif x[0] == 'remove':
+    #         y = int(x[1])
+    #         integer_set.remove(y)
 
-        else:
-            y = int(x[1])
-            integer_set.discard(y)
+    #     else:
+    #         y = int(x[1])
+    #         integer_set.discard(y)
     
-    print(integer_set)
+    # print(integer_set)
